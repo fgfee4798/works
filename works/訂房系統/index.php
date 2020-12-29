59b25b75188dc34a81b60407580ad784799941b7
guangy <fgfee4798@gmail.com>
2020-12-29 19:09:06 +0800

    版型/LOGO/導覽

---
+++ works/訂房系統/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+	
+
+	<title>光光訂房系統</title>
+	<link rel="stylesheet" href="">
+</head>
+
+<style>
+.container-fluid{background-color: #000000;height:100vh;}
+#top{background-color: #dab04c;
+	text-indent: -9999px;
+	height: 6px;
+}
+#top1{background-color: #393e46;
+	height: 130px;
+	text-align: center;
+	color: #FFFFFF;
+	padding: 30px;
+}	
+#nav{background-color: #222831;
+	height: 50px;
+	color: #FFFFFF;
+	text-align: center;
+	padding: 7px;
+	font-size:20px;
+	font-family: "微軟正黑體";
+}
+
+
+
+
+
+
+	
+#btn1 {
+  letter-spacing: 0;
+}
+
+#btn1:hover,
+#btn1:active {
+  letter-spacing: 5px;
+}
+
+#btn1:after,
+#btn1:before {
+  -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+  border: 1px solid rgba(255, 255, 255, 0);
+  bottom: 0px;
+  content: " ";
+  display: block;
+  margin: 0 auto;
+  position: relative;
+  -webkit-transition: all 280ms ease-in-out;
+          transition: all 280ms ease-in-out;
+  width: 0;
+}
+
+#btn1:hover:after,
+#btn1:hover:before {
+  -webkit-backface-visibility: hidden;
+          backface-visibility: hidden;
+  border-color: #fff;
+  -webkit-transition: width 350ms ease-in-out;
+          transition: width 350ms ease-in-out;
+  width: 70%;
+}
+
+#btn1:hover:before {
+  bottom: auto;
+  top: 0;
+  width: 70%;
+}
+
+
+
+</style>
+
+
+<body>
+	
+<div class="container-fluid">
+
+
+
+<div class="row" id="top">
+<div class="col-xs-12 col-md-12">光光訂房</div>
+</div>
+
+
+<div class="row" id="top1">
+<div class="hidden-xs col-md-4"></div>   
+<div class="col-xs-12 col-md-4"><a href="#"><img src="images/LOGO.png"></a></div>  
+<div class="hidden-xs col-md-4"></div>  
+</div>
+
+
+<div class="row" id="nav">
+<div class="col-xs-1 col-md-1 offset-3" id="btn1">回首頁 </div>
+<div class="col-xs-1 col-md-1" id="btn1">最新消息 </div>
+ <div class="col-xs-1 col-md-1" id="btn1">客房導覽</div>
+ <div class="col-xs-1 col-md-1" id="btn1"> 訂房須知 </div>
+  <div class="col-xs-1 col-md-1" id="btn1">我要訂房 </div>
+  <div class="col-xs-1 col-md-1" id="btn1">訂房查詢</div>
+</div>
+
+
+
+
+
+
+
+
+</div>
+
+
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</html>
