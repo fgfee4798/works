e91684a32015674a051b63b9f622dbc3af5c3efe
Xie Yiguang <74174355+fgfee4798@users.noreply.github.com>
2021-01-05 17:55:37 +0800

    Update connMysql.php

--- works/TTQS/connMysql.php
+++ works/TTQS/connMysql.php
@@ -1,8 +1,8 @@
 <?php
 	//資料庫主機設定
 	$db_host = "sql304.byethost17.com"; //預設3306
-	$db_username = "b17_27105184";
-	$db_password = "CHEN5660";
+	$db_username = "";
+	$db_password = "";
 	$db_name = "b17_27105184_TTQS";
 	//連線資料庫
 	$db_link = @new mysqli($db_host, $db_username, $db_password, $db_name);
