08cf05187ec6acd241d222d1541f507efdfe203d
shi <fgfee4798@gmail.com>
2021-01-13 17:55:25 +0800

    admin查詢系統完成

--- 訂房系統/admin_check.php
+++ 訂房系統/admin_check.php
@@ -3,14 +3,13 @@ require_once("mysql.php");
 session_start();
 
 //檢查是否經過登入
-/*if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
+if(!isset($_SESSION["loginMember"]) || ($_SESSION["loginMember"]=="")){
 	header("Location: admin_login.php");
-}*/
+}
 
 
 
-$new="SELECT * FROM new ORDER BY id ASC " ;
-$new_query=$db_link->query($new);
+
 
 
 
@@ -19,6 +18,37 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 	unset($_SESSION["loginMember"]);
 	header("Location: admin_login.php");
 }
+
+
+
+//查詢資料
+
+
+
+if(isset($_POST["action"]) && ($_POST["action"]=="show")){
+$search="SELECT number,name,reservation_style,reservation_date,reservation_money,remarks,id FROM reservation WHERE (reservation_date LIKE ?) AND  (name LIKE ?) AND  (number LIKE ?) ";
+
+$reservation_date ="%".$_POST["date_table"]."%";
+	$number="%".$_POST["number_table"]."%";
+	$name="%".$_POST["name_table"]."%";
+	$search_result=$db_link->prepare($search);
+	$search_result->bind_param("sss", $reservation_date,$name,$number);
+	$search_result->execute();
+	$search_result->bind_result($number,$name,$reservation_style,$reservation_date,$reservation_money,$remarks,$id) ;
+	$search_result->store_result();
+	//查詢筆數
+	$total_records=$search_result ->num_rows;
+
+	
+}
+
+
+
+
+
+
+
+
 
 ?>
 
@@ -29,11 +59,11 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 		
-		<title>管理系統</title>
+		<title>管理系統/訂房查詢</title>
 		<link rel="stylesheet" href="">
 	</head>
 	<style>
-	.container-fluid{background-color: #000000;height:100%;}
+	.container-fluid{height:100%;}
 	#top{background-color: #dab04c;
 		text-indent: -9999px;
 		height: 5px;
@@ -108,7 +138,10 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 		height: 100px;
 		position: relative;.
 	}
-			
+	#total_records{color:#c70039;
+font-size: 25px;
+	}	
+
 	</style>
 	<body>
 		
@@ -126,29 +159,75 @@ if(isset($_GET["logout"]) && ($_GET["logout"]=="true")){
 				<div class="col-xs-1 col-sm-1 text-nowrap" id="btn1"><a href="admin_check.php">訂房查詢</a> </div>
 				<div class="col-xs-1 col-sm-1 text-nowrap" id="btn1"><a href="admin_index.php?logout=true">登出</a> </div>
 			</div>
+			
+			
 			<div class="row">
-				<div class="col-xs-12  col-sm-3 offset-sm-3">
-					<p><h1>最新消息</h1></p>
-				</div>
-			</div>
-			<div class="row" >
-				<div class="col-xs-12 col-sm-6 offset-sm-3" id="newcolor">new</div>
-			</div>
-			<div class="row">
-				<div class="col-xs-8 col-sm-6 offset-sm-3">
-					<ul>
-						<?php while($new_query_e=$new_query->fetch_assoc()){ ;
+				<div class="col-md-8 offset-sm-2">
+					
+				<form action="admin_check.php" method="POST" accept-charset="utf-8">
+					<table class="table table-striped">
+					<tr><td>
+					日期:<input type="date" name="date_table" value="">
+					姓名:<input type="text" name="name_table" value="">
+					身分證字號:<input type="text" name="number_table" value="">
+					<input type="hidden" name="action" value="show">
+					<input type="submit" name="" value="搜尋" class="btn btn-primary">
+					</td></tr>
+				</table>
+			</form>
+		</div>
+</div>
 
-						echo "<a href='admin_index_delete.php?id=".$new_query_e["id"]."'>"."<button type='button' class='btn btn-light'>修改</button></a>&nbsp;" ;
-						echo "<a href='admin_index_delete.php?id=".$new_query_e["id"]."'>"."<button type='button' class='btn btn-light'>刪除</button></a>" ;
-						echo "<li><h2>".$new_query_e["new_date"]."</h2><h4>&nbsp;&nbsp;".$new_query_e["news"]."</h4></li>";
+<div class="row">
+	<div class="col-md-8 offset-sm-2" id="total_records">
+<?php
 
-						
-						
-						}?>
-					</ul>
-				</div>
-			</div>
+if(isset($_POST["action"])&& ($_POST["action"]=="show")){
+echo "查詢到".$total_records."筆";
+};
+?>
+</div>
+</div>
+<div class="row">
+				<div class="col-md-8 offset-sm-2">
+<table class="table table-striped table-hover">
+	
+	
+<tr><th>#</th><th>日期</th><th>姓名</th><th>身分證字號</th><th>房型</th><th>價錢</th><th>備註</th></tr>
+
+
+<?php
+if(isset($_POST["action"])&& ($_POST["action"]=="show")){
+		while($search_result->fetch()){
+		echo "<tr>" ;
+		echo "<td>".$id."</td>" ;
+		echo "<td>".$reservation_date."</td>" ;
+		echo "<td>".$name."</td>" ;
+		echo "<td>".$number."</td>" ;
+		echo "<td>".$reservation_style."</td>" ;
+		echo "<td>".$reservation_money."</td>" ;
+		echo "<td>".$remarks."</td>" ;
+		echo "</tr>" ;
+
+		 };
+		 
+	
+};	
+		
+		 ?>
+
+
+
+</table>
+
+
+
+
+	</div>
+</div>
+
+
+
 		</div>
 	</body>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
